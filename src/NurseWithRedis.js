173e0e42b357ccfde350781b03212f35881b360e
sak39 <dnsn.isk@gmail.com>
2016-11-05 19:02:50 +0900

    modify a header and es6 back to es5
    use REDISTOGO_URL to attach to Redis_to_Go.
    In hubot apps, es6 isn't handled.

--- src/NurseWithRedis.js
+++ src/NurseWithRedis.js
@@ -1,3 +1,9 @@
+"use strict";
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 /**
  * Created by pncity on 2016/10/25.
  * redisでは重複検査メソッドなどがデフォルトで存在する
@@ -6,143 +12,168 @@
 // let redis = require("redis"),
 //   client = redis.createClient();
 
-let redis = require("redis"), client;
+var redis = require("redis"),
+    client;
 
 if(process.env.REDISTOGO_URL !== undefined) {
-  redisInfo = require("url").parse(process.env.REDISTOGO_URL);
+  var redisInfo = require("url").parse(process.env.REDISTOGO_URL);
   client = redis.createClient(redisInfo.port, redisInfo.hostname);
   client.auth(redisInfo.auth.split(":")[1]);
 } else {
   client = redis.createClient();
 }
 
-console.log(client.host);
-console.log(client.port);
+var NurseWithRedis = function () {
+  function NurseWithRedis() {
+    _classCallCheck(this, NurseWithRedis);
+  }
 
-class NurseWithRedis {
-  // const key = channelKey || "key"; //default key is "sites"
-  /** Implement **/
-  /**
-   * getListAll: get hash array of all, url and statusCode.
-   *
-   * @param <none>
-   * @return <Hash> { 'http://yahoo.co.jp': '200', 'https://google.com': '200' } -> forEachで処理shori
-   */
-  static getListAll(key, callback) {
-    client.hgetall(key, callback);
-  };
+  _createClass(NurseWithRedis, null, [{
+    key: "getListAll",
 
-  /**
-   * getListKeys: get hash array of url names.
-   *
-   * @param <none>
-   * @return <Hash> { 'http://yahoo.co.jp', 'https://google.com' }
-   */
-  static getListKeys(key, callback) {
-    client.hkeys(key, callback);
-  };
+    /** Implement **/
+    /**
+     * getListAll: get hash array of all, url and statusCode.
+     *
+     * @param <none>
+     * @return <Hash> { 'http://yahoo.co.jp': '200', 'https://google.com': '200' } -> forEachで処理shori
+     */
+    value: function getListAll(key, callback) {
+      client.hgetall(key, callback);
+    }
+  }, {
+    key: "getListKeys",
 
-  /**
-   * addUrl: adding some url and status pairs into list.
-   *
-   * @param <Array, Function> dataArray, callback, msg (required) data array adding into list. msg is a Message object in hubot.
-   * @return <String>  Simple string reply (http://redis.io/topics/protocol#simple-string-reply)
-   */
-  static addUrl(key, dataArray, callback, msg) {
-    /* dataArray: 元のurl,statusのarray, _dataArray: 最終的にaddする要素を格納するarray, _urlArray: hkeysの検索結果のarray */
-    console.log("dataArray1: ", dataArray); //@@
 
-    //swap用
-    let _dataArray = [];
-    console.log("_dataArray1: ", _dataArray); //@@ 追加するURLsを格納するarray
+    /**
+     * getListKeys: get hash array of url names.
+     *
+     * @param <none>
+     * @return <Hash> { 'http://yahoo.co.jp', 'https://google.com' }
+     */
+    value: function getListKeys(key, callback) {
+      client.hkeys(key, callback);
+    }
+  }, {
+    key: "addUrl",
 
-    //eliminate existing elements from dataArray
-    client.hkeys(key, function (err, _urlArray) {
-      dataArray.forEach(function (url, i) {
-        console.log("i: " + i);
 
-        if(i%2 === 0) { //urlのみ対象
-          if (_urlArray.indexOf(url) > -1) { //exists
-            console.log("exists: true");
-            msg.send("Adding ERROR: '" + url + "' already exist.");
-          } else { //not exists
-            console.log("exists: false");
-            _dataArray.push(dataArray[i], dataArray[i+1]);
+    /**
+     * addUrl: adding some url and status pairs into list.
+     *
+     * @param <Array, Function> dataArray, callback, msg (required) data array adding into list. msg is a Message object in hubot.
+     * @return <String>  Simple string reply (http://redis.io/topics/protocol#simple-string-reply)
+     */
+    value: function addUrl(key, dataArray, callback, msg) {
+      /* dataArray: 元のurl,statusのarray, _dataArray: 最終的にaddする要素を格納するarray, _urlArray: hkeysの検索結果のarray */
+      console.log("dataArray1: ", dataArray); //@@
+
+      //swap用
+      var _dataArray = [];
+      console.log("_dataArray1: ", _dataArray); //@@ 追加するURLsを格納するarray
+
+      //eliminate existing elements from dataArray
+      client.hkeys(key, function (err, _urlArray) {
+        dataArray.forEach(function (url, i) {
+          console.log("i: " + i);
+
+          if (i % 2 === 0) {
+            //urlのみ対象
+            if (_urlArray.indexOf(url) > -1) {
+              //exists
+              console.log("exists: true");
+              msg.send("Adding ERROR: '" + url + "' already exist.");
+            } else {
+              //not exists
+              console.log("exists: false");
+              _dataArray.push(dataArray[i], dataArray[i + 1]);
+            }
           }
-        }
+        });
+
+        console.log("_dataArray2: ", _dataArray); //@@ 最終的に追加するuRL
+        client.hmset(key, _dataArray, callback);
       });
+    }
+  }, {
+    key: "updateUrl",
 
-      console.log("_dataArray2: ", _dataArray); //@@ 最終的に追加するuRL
-      client.hmset(key, _dataArray, callback);
-    });
-  };
 
-  /**
-   *  updateUrl: (deprecated) update status code of existing url in list.
-   *
-   *  @param <integer, integer, Function> index, status, callback (required) Pair of pertinent index and new status code.
-   *  @return <integer> 0: overwrite(success), 1: new create(failed)
-   *  */
-  static updateUrl(key, index, status, callback) {
-    let url;
-    client.multi()
-      .hkeys(key, function (err, _urlArray) {
+    /**
+     *  updateUrl: (deprecated) update status code of existing url in list.
+     *
+     *  @param <integer, integer, Function> index, status, callback (required) Pair of pertinent index and new status code.
+     *  @return <integer> 0: overwrite(success), 1: new create(failed)
+     *  */
+    value: function updateUrl(key, index, status, callback) {
+      var url = void 0;
+      client.multi().hkeys(key, function (err, _urlArray) {
         //もしindexがoverflowだとundefinedが代入され、undefined:statusで登録されてしまう
 
-        if(_urlArray[index] !== undefined) {
+        if (_urlArray[index] !== undefined) {
           url = _urlArray[index];
           client.hset(key, url, status, callback); //promiseで繋げたい
-        } else { //index is overflow
+        } else {
+          //index is overflow
           callback({}, null);
         }
-      })
-      .exec();
-  };
+      }).exec();
+    }
+  }, {
+    key: "removeUrl",
 
-  /**
-   * removeUrl: remove some url and status pairs from list.
-   *
-   * @param <integer, Function> index, callback (required) indexes wanted to be removed from list.
-   * @return <integer> the number of removed elements.
-   */
-  /* remove */
-  static removeUrl(key, index, callback) {
-    /* 複数Urlに対応 */
-    //対応しようと思ったのだが、hdelはarrayでフィールド指定ができないため一括削除することをデフォルトで想定していないと見た。
 
-    let url; //削除するurl(swap用)
-    client.multi()
-      .hkeys(key, function (err, _urlArray) {
+    /**
+     * removeUrl: remove some url and status pairs from list.
+     *
+     * @param <integer, Function> index, callback (required) indexes wanted to be removed from list.
+     * @return <integer> the number of removed elements.
+     */
+    /* remove */
+    value: function removeUrl(key, index, callback) {
+      /* 複数Urlに対応 */
+      //対応しようと思ったのだが、hdelはarrayでフィールド指定ができないため一括削除することをデフォルトで想定していないと見た。
+
+      var url = void 0; //削除するurl(swap用)
+      client.multi().hkeys(key, function (err, _urlArray) {
         //本来ならばここで_urlArray[index] !== undefined の判定が必要だが、これをあえて見逃すことでundefinedというfiledを削除してくれるため一石二鳥であるから判定は加えない。
-          url = _urlArray[index];
-          client.hdel(key, url, callback); //これをmultiで繋げたい
-      })
-      .exec();
-  };
+        url = _urlArray[index];
+        client.hdel(key, url, callback); //これをmultiで繋げたい
+      }).exec();
+    }
+  }, {
+    key: "searchIndexFromUrl",
 
-  /**
-   *  searchIndexIndexFromUrl: Return a index number of specified url.
-   *
-   * @param <String, Function> url(required), callback(required) Specified url
-   * @return <integer> index number, or -1 if not exists.
-   */
-  static searchIndexFromUrl(key, url, callback) {
-    client.hkeys(key, function (err, _urlArray) {
-      callback(_urlArray.indexOf(url));
-    });
-  };
 
-  /**
-   *  searchUrlFromIndex: Return a url name of specified index.
-   *
-   * @param <integer, Function> index(required), callback(required) index number of url
-   * @return <String> url name, or -1 if not exists.
-   */
-  static searchUrlFromIndex(key, index, callback) {
-    client.hkeys(key, function (err, _urlArray) {
-      callback(_urlArray[index]);
-    });
-  };
-}
+    /**
+     *  searchIndexIndexFromUrl: Return a index number of specified url.
+     *
+     * @param <String, Function> url(required), callback(required) Specified url
+     * @return <integer> index number, or -1 if not exists.
+     */
+    value: function searchIndexFromUrl(key, url, callback) {
+      client.hkeys(key, function (err, _urlArray) {
+        callback(_urlArray.indexOf(url));
+      });
+    }
+  }, {
+    key: "searchUrlFromIndex",
+
+
+    /**
+     *  searchUrlFromIndex: Return a url name of specified index.
+     *
+     * @param <integer, Function> index(required), callback(required) index number of url
+     * @return <String> url name, or -1 if not exists.
+     */
+    value: function searchUrlFromIndex(key, index, callback) {
+      client.hkeys(key, function (err, _urlArray) {
+        callback(_urlArray[index]);
+      });
+    }
+  }]);
+
+  return NurseWithRedis;
+}();
 
 module.exports.NurseWithRedis = NurseWithRedis;
